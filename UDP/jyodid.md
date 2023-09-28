d296233340d078e25481f3d876e06e4198325e50
Sreelaxme <sreeluvkv@gmail.com>
2023-09-29 04:58:51 +0530

    uap.py done ig

---
+++ UDP/jyodid.md
@@ -0,0 +1,2 @@
+### THIS IS WHAT JYO TOLD....IK NO SHIT
+- uap.py has the things to do encoding and decoding stuff...chatgpt and jyo did it
--- UDP/uap.py
+++ UDP/uap.py
@@ -1,3 +1,5 @@
+import struct
+
 class UAP:
 
     class CommandEnum:
@@ -5,6 +7,8 @@ class UAP:
         DATA = 1
         ALIVE = 2
         GOODBYE = 3
+
+    MAGIC_NUMBER = 0xC461 
 
     def __init__(self, magic, version, command, sequence, session, message):
         self.magic = magic
@@ -15,41 +19,24 @@ class UAP:
         self.message = message
 
     def encode(self):
-        header = (
-            (self.magic << 96) |
-            (self.version << 68) |
-            (self.command << 40) |
-            (self.sequence << 8) |
-            self.session
-        )
-        return header.to_bytes(24, 'big') + self.message.encode()
+        header = struct.pack('>QIBIQ', self.magic, self.version, self.command, self.sequence, self.session)
+        return header + self.message.encode()
 
     @classmethod
     def decode(cls, data):
+        print(data)
+        if len(data) != 25:
+            raise ValueError("Input data must have 25 bytes to unpack.")
+        
         header_bytes = data[:24]
         message_bytes = data[24:]
         
-        header = int.from_bytes(header_bytes, 'big')
-        
-        magic = header >> 96
-        version = (header >> 68) & 0xFF
-        command = (header >> 40) & 0xFF
-        sequence = (header >> 8) & 0xFFFFFFFF
-        session = header & 0xFFFFFFFF
+        magic, version, command, sequence, session = struct.unpack('>QIBIQ', header_bytes)
         
         message = message_bytes.decode('utf-8')
         
         return cls(magic, version, command, sequence, session, message)
 
-    def __str__(self):
-        return (
-            f"Magic: {self.magic}\n"
-            f"Version: {self.version}\n"
-            f"Command: {self.command}\n"
-            f"Sequence: {self.sequence}\n"
-            f"Session: {self.session}\n"
-            f"Message: {self.message}\n"
-        )
 
 class Message:
 
@@ -61,67 +48,36 @@ class Message:
         self.version = 1
         self.magic = UAP.MAGIC_NUMBER
 
-    def __str__(self):
-        return (
-            f"| Magic: {self.magic} "
-            f"| Version: {self.version} "
-            f"| Command: {self.command} "
-            f"| Sequence: {self.sequence_no} "
-            f"| Session: {self.session_id} "
-            f"| Message: {self.message} |"
-        )
-
-    def __repr__(self):
-        return str(self)
-
-    @staticmethod
-    def encode_message(command, sequence_no, session_id, message):
-        header_num = UAP.MAGIC_NUMBER << UAP.SHIFTS["MAGIC"]
-        header_num += 1 << UAP.SHIFTS["VERSION"]
-        header_num += command << UAP.SHIFTS["COMMAND"]
-        header_num += sequence_no << UAP.SHIFTS["SEQUENCE"]
-        header_num += session_id << UAP.SHIFTS["SESSION"]
-
-        return header_num.to_bytes(24, 'big') + message.encode()
-
     def encode(self):
-        return self.encode_message(self.command, self.sequence_no, self.session_id, self.message)
+        header = struct.pack('>QIBIQ', self.magic, self.version, self.command, self.sequence_no, self.session_id)
+        return header + self.message.encode()
 
     @classmethod
-    def decode_message(cls, message_bytes):
-        message_num = int.from_bytes(message_bytes, "big")
-
-        magic = UAP.MagicBits(message_num)
-        version = UAP.VersionBits(message_num)
-
-        if magic != UAP.MAGIC_NUMBER:
-            raise ValueError(f"Magic number does not match. Obtained magic number: {magic}")
-
-        if version != 1:  # This is a v1 agent
-            raise ValueError(f"Version mismatch. Obtained version: {version}")
-
-        command = UAP.CommandBits(message_num)
-        sequence_no = UAP.SequenceBits(message_num)
-        session_id = UAP.SessionBits(message_num)
-        message_str = (UAP.MessageBits(message_num).to_bytes(message_num.bit_length() // 4 + 1, "big")).decode("utf-8")
-
-        return cls(command, sequence_no, session_id, message_str)
-
-    @classmethod
-    def decode(cls, message_bytes):
-        return cls.decode_message(message_bytes)
-
+    def decode(cls, data):
+        if len(data) < 25:
+            raise ValueError("Input data must have 25 bytes to unpack.")
+        
+        header_bytes = data[:25]
+        message_bytes = data[25:]
+        
+        magic, version, command, sequence, session = struct.unpack('>QIBIQ', header_bytes)
+        
+        message = message_bytes.decode('utf-8')
+        
+        return cls(command, sequence, session, message)
+    
+    def __str__(self):
+        return (f"| {self.magic} | {self.version} | {self.command} | {self.sequence_no} | {self.session_id} | {self.message} |")
 
 if __name__ == "__main__":
-
     m = Message(1, 0, 0, "Hello world")
 
     print("Input Message:")
     print(m)
 
-    m_enc = m.EncodeMessage()
+    m_enc = m.encode()
 
-    m_dec = Message.DecodeMessage(m_enc)
+    m_dec = Message.decode(m_enc)
 
     print("Output Message:")
     print(m_dec)
