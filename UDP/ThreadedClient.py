928edc88e53e7fb19bce1a1221be1a0342433895
lechurnair <75621929+lechurnair@users.noreply.github.com>
2023-10-06 00:54:42 +0530

    Update ThreadedClient.py

--- UDP/ThreadedClient.py
+++ UDP/ThreadedClient.py
@@ -79,13 +79,13 @@ if __name__ == "__main__":
         while currState in [STATES["Ready"],STATES["Ready Timer"]]:
             try:
                 m  = input()
+                message = m.encode('utf-8',errors="ignore")
+                m = message.decode('utf-8')  
                 if(m == None or len(m)==0):
                     continue
                 if(m == "eof"):   
                     currState = STATES["Closing"]
-                    break
-                message = m.encode('utf-8',errors="ignore")
-                m = message.decode('utf-8')   
+                    break 
             except EOFError:
                 currState = STATES["Closing"]
                 break
